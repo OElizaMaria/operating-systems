f7c50fef2fd1fedea613cf2e0a576769577f5cae
Cosmin Rasadeanu <cosmin.rasadeanu@gmail.com>
2026-01-17 12:58:36 +0200

    getting somewhere

--- content/assignments/local-procedure-call/src/dispatcher.c
+++ content/assignments/local-procedure-call/src/dispatcher.c
@@ -15,24 +15,30 @@ typedef struct ServiceArgs {
     pthread_mutex_t mutex;
     int service_fd;
     char service_fifo[32];
+    char buffer[64];
 } ServiceArgs;
 
 void server_thread(ServiceArgs *args) {
     struct InstallRequestHeader IRH;
     pthread_mutex_lock(&args->mutex);
     strcpy(args->service_fifo, "../tests/.dispatcher/install_req_pipe");
-    mkfifo(args->service_fifo, 0666);
-    args->service_fd = open(args->service_fifo, O_RDONLY);
-    read(args->service_fd, &IRH, sizeof(struct InstallRequestHeader));
-    IRH.m_IpnLen = be32toh(IRH.m_IpnLen);
-    printf("%s\n", args->service_fifo);
-    pthread_mutex_unlock(&args->mutex);
-
+    while (true) {
+        mkfifo(args->service_fifo, 0666);
+        args->service_fd = open(args->service_fifo, O_RDONLY);
+        while (open(args->service_fifo, O_RDONLY)) {
+            read(args->service_fd, &IRH, sizeof(struct InstallRequestHeader));
+            // IRH.m_IpnLen = be32toh(IRH.m_IpnLen);
+            read(args->service_fd, args->buffer, IRH.m_IpnLen);
+            printf("buffer: %s\n", args->buffer);
+            close(args->service_fd);
+            unlink(args->service_fifo);
+        }
+        pthread_mutex_unlock(&args->mutex);
+    }
 }
 
 int main(void)
 {
-    // threads are just memory
     ServiceArgs *args = malloc(sizeof(ServiceArgs));
     pthread_mutex_init(&args->mutex, 0);
     pthread_t *service_thread = malloc(sizeof(pthread_t));
